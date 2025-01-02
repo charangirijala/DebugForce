9c6fe9c6a480cc90f5991c78ea7ce324894d71d6
Charan Girijala <82193104+charangirijala@users.noreply.github.com>
2025-01-02 08:24:17 +0530

    Site logo added👀

--- scripts/copy-slds.mjs
+++ scripts/copy-slds.mjs
@@ -1,9 +1,13 @@
-import fs from "fs";
+import fs from 'fs';
 fs.cp(
-  "./node_modules/@salesforce-ux/design-system/assets/",
-  "./src/assets",
-  { recursive: true },
-  () => {
-    console.log("Done copying SLDS resources");
-  }
+    './node_modules/@salesforce-ux/design-system/assets/',
+    './src/assets',
+    { recursive: true },
+    () => {
+        console.log('Done copying SLDS resources');
+    }
 );
+
+fs.cp('src/logo', './src', { recursive: true }, () => {
+    console.log('Done copying logos');
+});
