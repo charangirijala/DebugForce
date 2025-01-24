703270805dac9491601e203621496ec92c878334
Charan Girijala <82193104+charangirijala@users.noreply.github.com>
2025-01-24 12:05:24 +0530

    Dark mode impl-2 🌘

--- src/modules/my/app/app.js
+++ src/modules/my/app/app.js
@@ -1,61 +1,31 @@
 import { LightningElement } from 'lwc';
 import { initTheme } from 'services/theme';
-
 export default class App extends LightningElement {
+    theme;
     connectedCallback() {
-        initTheme();
-        // window.addEventListener('scroll', this.handleScroll);
+        this.theme = initTheme();
+        window.addEventListener('scroll', this.handleScroll);
     }
-
     disconnectedCallback() {
-        // window.removeEventListener('scroll', this.handleScroll);
+        window.removeEventListener('scroll', this.handleScroll);
     }
 
     handleScroll = () => {
         const firstHeader = this.template.querySelector('.first-header');
         const secondHeader = this.template.querySelector('.second-header');
-
-        const firstHeaderLogo =
-            this.template.querySelector('#first-header-logo');
-        const secondHeaderLogoContainer = this.template.querySelector(
-            '#second-header-logo'
-        );
-        console.log(
-            'headers: ',
-            firstHeader,
-            '2nd: ',
-            secondHeader,
-            '1st logo: ',
-            firstHeaderLogo,
-            ' 2nd logo: ',
-            secondHeaderLogoContainer
-        );
-        if (firstHeader && secondHeader) {
-            console.log('scroll: ', window.scrollY);
+        const logoContainer = this.template.querySelector('.logo-container');
+        const content = this.template.querySelector('.content');
+        if (firstHeader && secondHeader && logoContainer && content) {
             if (window.scrollY > 0) {
-                // Hide the first header
+                this.headerMinimized = true;
                 firstHeader.classList.add('hidden');
-
-                // Move the second header to the top
                 secondHeader.classList.add('move-to-top');
-
-                // // Move the logo to the second header
-                // if (!secondHeaderLogoContainer.querySelector('.logo')) {
-                //     const clonedLogo = firstHeaderLogo.cloneNode(true);
-                //     clonedLogo.classList.add('move-to-second-header'); // Add animation class
-                //     secondHeaderLogoContainer.appendChild(clonedLogo);
-                //}
+                logoContainer.style.display = 'inline-block';
             } else {
-                // Show the first header
+                this.headerMinimized = false;
                 firstHeader.classList.remove('hidden');
-
-                // Move the second header back to its default position
                 secondHeader.classList.remove('move-to-top');
-
-                // Remove the logo from the second header
-                // if (secondHeaderLogoContainer.querySelector('.logo')) {
-                //     secondHeaderLogoContainer.innerHTML = '';
-                // }
+                logoContainer.style.display = 'none';
             }
         }
     };
